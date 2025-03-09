363a935109319d4846c8615df4d6ba74894dc623
Polyacetone <mitsuha.2213@gmail.com>
2025-03-09 09:12:16 +0800

    autoaim_launcher新增出错自动重启

--- src/autoaim_launcher/launch/autoaim_launcher.py
+++ src/autoaim_launcher/launch/autoaim_launcher.py
@@ -1,55 +1,66 @@
-import os
-from launch import LaunchDescription
-from launch_ros.actions import ComposableNodeContainer
+import launch, time, os
+from launch_ros.actions import ComposableNodeContainer, LoadComposableNodes
 from launch_ros.descriptions import ComposableNode
-from launch_ros.substitutions import FindPackageShare
-from launch.actions import Shutdown, SetEnvironmentVariable, IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.event_handlers import (OnExecutionComplete, OnProcessExit, OnProcessIO, OnProcessStart, OnShutdown)
+from launch.actions import RegisterEventHandler, LogInfo, OpaqueFunction, SetEnvironmentVariable
 from ament_index_python.packages import get_package_share_directory
 
-use_intra_process_comms = True
+def launch_func(context, *args, **kwargs):
+    camera_params_yaml = os.path.join(get_package_share_directory('autoaim_camera'), 'config', 'params.yaml')
+    detection_params_yaml = os.path.join(get_package_share_directory('autoaim_detection'), 'config', 'params.yaml')
+    prediction_params_yaml = os.path.join(get_package_share_directory('autoaim_prediction'), 'config', 'params.yaml')
+    composable_node_descriptions = [
+        ComposableNode(
+            package='autoaim_camera',
+            plugin='autoaim_camera::CameraNode',
+            name='autoaim_camera',
+            parameters=[camera_params_yaml],
+            extra_arguments=[{'use_intra_process_comms': True}]
+        ),
+        ComposableNode(
+            package='autoaim_detection',
+            plugin='autoaim_detection::YoloDetectNode',
+            name='autoaim_detection',
+            parameters=[detection_params_yaml],
+            extra_arguments=[{'use_intra_process_comms': True}]
+        ),
+        ComposableNode(
+            package='autoaim_prediction',
+            plugin='autoaim_prediction::PredictionNode',
+            name='autoaim_prediction',
+            parameters=[prediction_params_yaml],
+            extra_arguments=[{'use_intra_process_comms': True}]
+        )
+    ]
+    time.sleep(1)
+    return [
+        LoadComposableNodes(
+            composable_node_descriptions=composable_node_descriptions, 
+            target_container='autoaim_container'
+        )
+    ]
+
 
 def generate_launch_description():
     set_env_log_format = SetEnvironmentVariable(
         name='RCUTILS_CONSOLE_OUTPUT_FORMAT',
         value='[{severity}] [{name}]: {message}'
     )
-
-    camera_params_yaml = os.path.join(get_package_share_directory('autoaim_camera'), 'config', 'params.yaml')
-    detection_params_yaml = os.path.join(get_package_share_directory('autoaim_detection'), 'config', 'params.yaml')
-    prediction_params_yaml = os.path.join(get_package_share_directory('autoaim_prediction'), 'config', 'params.yaml')
-    
     container = ComposableNodeContainer(
-        name='autoaim',
+        name='autoaim_container',
         namespace='',
         package='rclcpp_components',
         executable='component_container_mt',
-        composable_node_descriptions=[
-            ComposableNode(
-                package='autoaim_camera',
-                plugin='autoaim_camera::CameraNode',
-                name='autoaim_camera',
-                parameters=[camera_params_yaml],
-                extra_arguments=[{'use_intra_process_comms': use_intra_process_comms}]
-            ),
-            ComposableNode(
-                package='autoaim_detection',
-                plugin='autoaim_detection::YoloDetectNode',
-                name='autoaim_detection',
-                parameters=[detection_params_yaml],
-                extra_arguments=[{'use_intra_process_comms': use_intra_process_comms}]
-            ),
-            ComposableNode(
-                package='autoaim_prediction',
-                plugin='autoaim_prediction::PredictionNode',
-                name='autoaim_prediction',
-                parameters=[prediction_params_yaml],
-                extra_arguments=[{'use_intra_process_comms': use_intra_process_comms}]
-            )
-        ],
         output='both',
         emulate_tty=True,
-        on_exit=Shutdown(),
+        respawn=True,
+        respawn_delay=3
+    )
+    event = RegisterEventHandler(
+        OnProcessStart(
+            target_action=container,
+            on_start=[LogInfo(msg='autoaim container started'), OpaqueFunction(function=launch_func)]
+        )
     )
 
-    return LaunchDescription([set_env_log_format, container])
+    return launch.LaunchDescription([set_env_log_format, event, container])
